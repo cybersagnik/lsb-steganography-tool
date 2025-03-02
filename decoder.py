3cc925955f232f469ad8e611947c60ecbce16f35
cybersagnik <raysagnik86@gmail.com>
2025-03-02 18:50:01 +0530

    Update decoder.py
    Supporting decryption and cli enhancements

--- decoder.py
+++ decoder.py
@@ -1,23 +1,36 @@
+from tqdm import tqdm
 from image_processor import ImageProcessor
+from encrypy_decrypt import AESCipher
 
-class Decoder :
-	def __init__(self , image_processor: ImageProcessor):
-		"""Initialize the Decoder with an ImageProcessor instance"""
-		self.image_processor = image_processor
-	def extract_message(self):
-		"""Extract the embedded text message from the least significant bits of the image."""
-		flat_pixels = self.image_processor.pixel_matrix.flatten()
-		binary_message = ''
+class Decoder:
+    def __init__(self, image_processor: ImageProcessor,encrypt_key: str):
+        """Initialize Decoder with an ImageProcessor instance."""
+        if not isinstance(image_processor, ImageProcessor):
+            raise TypeError("Expected an instance of ImageProcessor.")
+        self.image_processor = image_processor
+        self.aes_cipher = AESCipher(encrypt_key)
 
-		for pixels in flat_pixels:
-			binary_message += str(pixels & 1) #Extract the LSB
+    def extract_message(self):
+        """Extracts the hidden message from the LSB of the image."""
+        try:
+            if self.image_processor.pixel_matrix is None:
+                raise RuntimeError("❌ Error: Image not loaded. Please load an image first.")
 
-		#Split binary bits into 8-bit chunks
-		byte_chars = [binary_message[i:i+8] for i in range(0 , len(binary_message),8)]
+            flat_pixels = self.image_processor.pixel_matrix.flatten()
+            binary_message = ''.join(str(pixel & 1) for pixel in tqdm(flat_pixels, desc="Extracting", unit="bit"))
 
-		extracted_message = ''
-		for byte in byte_chars:
-			if byte == '11111111':
-				break
-			extracted_message += chr(int(byte,2))
-		return extracted_message
+            end_index = binary_message.find('1111111111111110') #16-bit 1111111111111110 #8-bit 11111111
+            if end_index == -1:
+                raise ValueError("❌ Error: No valid hidden message found.")
+
+            binary_message = binary_message[:end_index]
+            encrypted_decoded_message = ''.join(chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8))
+            decrypted_message = self.aes_cipher.decrypt_message(encrypted_decoded_message)
+
+            print("✅ Hidden Message Extracted Successfully:")
+            print(f"📜 {decrypted_message}")
+            return decrypted_message
+
+        except Exception as e:
+            print(f"⚠️ Unexpected Error: {e}")
+            return None
